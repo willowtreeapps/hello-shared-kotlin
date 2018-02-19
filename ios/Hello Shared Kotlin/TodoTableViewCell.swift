6fefeb238cba0e8f42d50c38fd5b2b8ce85c3061
Erik LaManna <erik.lamanna@willowtreeapps.com>
2018-02-19 11:34:42 -0500

    Update design to add new items

--- ios/Hello Shared Kotlin/TodoTableViewCell.swift
+++ ios/Hello Shared Kotlin/TodoTableViewCell.swift
@@ -11,20 +11,59 @@ import KotlinHello
 
 class TodoTableViewCell : UITableViewCell {
     //MARK: Properties
-    @IBOutlet var todoText: UILabel!
-    @IBOutlet var done: UISwitch!
+    
+    @IBOutlet weak var addIcon: UIImageView!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var todoTextField: UITextField!
+
     var store: KotlinHelloAppStore!
     var index: Int = -1
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        todoTextField.delegate = self
+    }
     
     func bind(store: KotlinHelloAppStore, state: KotlinHelloAppState, index: Int) {
         self.store = store
         self.index = index
-        let todo = state.todos[index]
-        todoText.text = todo.text
-        done.isOn = todo.done
+        
+        if index < state.todos.count {
+            let todo = state.todos[index]
+            todoTextField.text = todo.text
+            checkButton.isSelected = todo.done
+            checkButton.isHidden = false
+            addIcon.isHidden = true
+        } else {
+            todoTextField.text = ""
+            checkButton.isSelected = false
+            checkButton.isHidden = true
+            addIcon.isHidden = false
+        }
     }
     
-    @IBAction func doneChanged(_ sender: Any) {
+    @IBAction func checkTapped(_ sender: Any) {
+        checkButton.isSelected = !checkButton.isSelected
         store.dispatch(action: KotlinHelloActionCheck(index: Int32(index)))
+    }
+}
+
+extension TodoTableViewCell: UITextFieldDelegate {
+    
+    func updateTodoItem(text: String?) {
+        if index >= store.state.todos.count {
+            if let text = text {
+                store.dispatch(action: KotlinHelloActionAdd(text: text))
+            }
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateTodoItem(text: textField.text)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        updateTodoItem(text: textField.text)
+        return true
     }
 }
